a895980115c8b5dc980e7bfcbfb909e731970b13
Tabassum <tabassuml@joblogic.com>
2021-06-28 23:39:35 +0500

    working on movie list adapter

--- app/src/main/java/com/tabi/tmdbexplorer/data/remote/models/Movie.kt
+++ app/src/main/java/com/tabi/tmdbexplorer/data/remote/models/Movie.kt
@@ -9,13 +9,13 @@ data class Movie(
     @SerializedName("genre_ids") val genre_ids : List<String>,
     @SerializedName("id") val id : Int,
     @SerializedName("original_language") val original_language : String,
-    @SerializedName("original_title") val original_title : String,
+    @SerializedName("original_title") val originalTitle : String,
     @SerializedName("overview") val overview : String,
     @SerializedName("popularity") val popularity : Double,
-    @SerializedName("poster_path") val poster_path : String,
-    @SerializedName("release_date") val release_date : String,
+    @SerializedName("poster_path") val posterPath : String,
+    @SerializedName("release_date") val releaseDate : String,
     @SerializedName("title") val title : String,
     @SerializedName("video") val video : Boolean,
-    @SerializedName("vote_average") val vote_average : Double,
-    @SerializedName("vote_count") val vote_count : Int
+    @SerializedName("vote_average") val voteAverage : Double,
+    @SerializedName("vote_count") val voteCount : Int
 )
--- app/src/main/java/com/tabi/tmdbexplorer/ui/search/adapter/MovieViewModel.kt
+++ app/src/main/java/com/tabi/tmdbexplorer/ui/search/adapter/MovieViewModel.kt
@@ -1,9 +1,11 @@
 package com.tabi.tmdbexplorer.ui.search.adapter
 
+import androidx.lifecycle.MutableLiveData
 import com.tabi.tmdbexplorer.data.remote.models.Movie
 
-class MovieViewModel (val movie : Movie) {
-
-
-
+class MovieViewModel(val movie: Movie) {
+    var title: MutableLiveData<String> = MutableLiveData(movie.title)
+    var year: MutableLiveData<String> = MutableLiveData(movie.releaseDate)
+    var overview: MutableLiveData<String> = MutableLiveData(movie.overview)
+    var posterPath: MutableLiveData<String> = MutableLiveData(movie.posterPath)
 }
